67e72eeadddbf79ea31844b034e2a2dfadd6d613
Supraja <supra556699@gmail.com>
2024-11-25 02:27:43 +0800

    project

--- frontend.sh
+++ frontend.sh
@@ -53,7 +53,7 @@ cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>$LOG_FILE
 VALIDATE $? "Extract frontend code"
 
-cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf
+cp /home/ec2-user/project-shell/expense.conf /etc/nginx/default.d/expense.conf
 VALIDATE $? "Copied expense conf"
 
 systemctl restart nginx &>>$LOG_FILE
