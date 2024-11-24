61b8621f35abb38071a5cbf38de1a94852aec45f
Supraja <supra556699@gmail.com>
2024-11-25 02:31:04 +0800

    project

--- backend.sh
+++ backend.sh
@@ -65,14 +65,14 @@ unzip /tmp/backend.zip &>>$LOG_FILE
 VALIDATE $? "Extracting backend application code"
 
 npm install &>>$LOG_FILE
-cp /home/ec2-user/project-shell/backend.service /etc/systemd/system/backend.service
+cp /home/ec2-user/project-shell/backend.service/etc/systemd/system/backend.service
 
 # load the data before running backend
 
 dnf install mysql -y &>>$LOG_FILE
 VALIDATE $? "Installing MySQL Client"
 
-mysql -h mysql.daws81s.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE
+mysql -h mysql.daws81svardhanglobal.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE
 VALIDATE $? "Schema loading"
 
 systemctl daemon-reload &>>$LOG_FILE
